cb1662661a150d37f400f69a4e156756b6afe7a5
Ananth <ananth@functor.systems>
2025-11-04 21:51:59 -0500

    build(nix): setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "MCDP devshell setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { self, nixpkgs, devshell }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ devshell.overlays.default ];
+      };
+    in {
+      devShells.${system}.default = pkgs.devshell.mkShell {
+        name = "mcdp";
+
+        packages = with pkgs; [
+          git
+          bashInteractive
+        ];
+
+        # add ./bin to PATH
+        env = [
+          {
+            name = "PATH";
+            prefix = "${self}/bin";
+          }
+        ];
+      };
+    };
+}
